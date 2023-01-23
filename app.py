d0b4dbbb87528baece689e4906e67ca0cc36f897
BratkovskyEvgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-01-23 03:40:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-
+@st.cache
 def detect_objects(our_image):
     st.set_option('deprecation.showPyplotGlobalUse', False)
 
